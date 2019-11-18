f8612067cc443dc12103cd6d852156daf93ee151
IAmTerror <cyrilberthout@gmail.com>
2019-11-18 12:41:04 +0100

    leagues GB / IT / DE / ES

--- leagues.py
+++ leagues.py
@@ -9,8 +9,44 @@ def get_all_leagues():
     print(datas)
 
 
+# France
 def get_all_fr_leagues_by_code():
     url = "https://server1.api-football.com/leagues/country/FR"
     response = requests.request("GET", url, headers=HEADERS)
     datas = response.json()
     print(datas)
+
+
+# England / Great Britain
+def get_all_gb_leagues_by_code():
+    url = "https://server1.api-football.com/leagues/country/GB"
+    response = requests.request("GET", url, headers=HEADERS)
+    datas = response.json()
+    print(datas)
+
+
+# Spain
+def get_all_es_leagues_by_code():
+    url = "https://server1.api-football.com/leagues/country/ES"
+    response = requests.request("GET", url, headers=HEADERS)
+    datas = response.json()
+    print(datas)
+
+
+# Germany
+def get_all_de_leagues_by_code():
+    url = "https://server1.api-football.com/leagues/country/DE"
+    response = requests.request("GET", url, headers=HEADERS)
+    datas = response.json()
+    print(datas)
+
+
+# Italy
+def get_all_it_leagues_by_code():
+    url = "https://server1.api-football.com/leagues/country/IT"
+    response = requests.request("GET", url, headers=HEADERS)
+    datas = response.json()
+    print(datas)
+
+
+
--- main.py
+++ main.py
@@ -13,9 +13,17 @@ print("-------------------------------------------------------------------------
 if limit < 100:
     # get_top_scorers_in_ligue_1_fr_2018()
     # get_top_scorers_in_ligue_1_fr_2019()
+    # get_all_countries()
     # get_all_leagues()
     # get_all_fr_leagues_by_code()
-    get_all_countries()
+    # get_all_gb_leagues_by_code()
+    # get_all_es_leagues_by_code()
+    # get_all_de_leagues_by_code()
+    get_all_it_leagues_by_code()
+
+else:
+    print("No way : limit request per day reached")
+
 
 
 
