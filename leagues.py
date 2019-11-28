a96a1408c4d7dc3ba8775ade1520d65f0ccafcf0
IAmTerror <cyrilberthout@gmail.com>
2019-11-28 19:27:51 +0100

    generalization ---> get_all_country_leagues_by_code(code)

--- leagues.py
+++ leagues.py
@@ -16,41 +16,8 @@ def get_league_by_code_and_season(country, season):
     print(datas)
 
 
-# France
-def get_all_fr_leagues_by_code():
-    url = "https://server1.api-football.com/leagues/country/FR"
-    response = requests.request("GET", url, headers=HEADERS)
-    datas = response.json()
-    print(datas)
-
-
-# England / Great Britain
-def get_all_gb_leagues_by_code():
-    url = "https://server1.api-football.com/leagues/country/GB"
-    response = requests.request("GET", url, headers=HEADERS)
-    datas = response.json()
-    print(datas)
-
-
-# Spain
-def get_all_es_leagues_by_code():
-    url = "https://server1.api-football.com/leagues/country/ES"
-    response = requests.request("GET", url, headers=HEADERS)
-    datas = response.json()
-    print(datas)
-
-
-# Germany
-def get_all_de_leagues_by_code():
-    url = "https://server1.api-football.com/leagues/country/DE"
-    response = requests.request("GET", url, headers=HEADERS)
-    datas = response.json()
-    print(datas)
-
-
-# Italy
-def get_all_it_leagues_by_code():
-    url = "https://server1.api-football.com/leagues/country/IT"
+def get_all_country_leagues_by_code(code):
+    url = "https://server1.api-football.com/leagues/country/" + code
     response = requests.request("GET", url, headers=HEADERS)
     datas = response.json()
     print(datas)
--- main.py
+++ main.py
@@ -11,15 +11,11 @@ print("-------------------------------------------------------------------------
 print("---------------------------------------------------------------------------------------------------------------")
 
 if limit < 100:
-    get_top_scorers_by_league_id("2")
+    # get_top_scorers_by_league_id("2")
     # get_all_countries()
     # get_all_leagues()
     # get_league_by_code_and_season("GB", "2018")
-    # get_all_fr_leagues_by_code()
-    # get_all_gb_leagues_by_code()
-    # get_all_es_leagues_by_code()
-    # get_all_de_leagues_by_code()
-    # get_all_it_leagues_by_code()
+    get_all_country_leagues_by_code("FR")
 
 else:
     print("No way : limit request per day reached")
